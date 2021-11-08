dad5a97789ecdaa1c7abc71f0b945b48560cf745
HJ Miranda <hjmiranda02@gmail.com>
2021-11-08 22:18:38 +0800

    contact changed and error pages done

--- resources/views/errors/429.blade.php
+++ resources/views/errors/429.blade.php
@@ -2,4 +2,4 @@
 
 @section('title', __('Too Many Requests'))
 @section('code', '429')
-@section('message', __('Too Many Requests'))
+@section('message', __('The user sent too many requests in a given amount of time.'))
