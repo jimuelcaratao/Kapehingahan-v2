19b55793fa416ffca4142f598a0a9695bd505b2c
HJ Miranda <hjmiranda02@gmail.com>
2022-02-11 16:16:32 +0800

    login and register, contact and about us

--- resources/views/pages/ecommerce/about.blade.php
+++ resources/views/pages/ecommerce/about.blade.php
@@ -1,5 +1,10 @@
 <x-ecommerce-layout>
 
+                {{-- tailwind cdn --}}
+                <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+                {{-- tailwind script --}}
+                <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+
     <x-slot name="title">
         About |
     </x-slot>
@@ -7,6 +12,9 @@
     {{-- Logo --}}
     <div class="container px-10 py-5 mx-auto flex flex-wrap items-center justify-center">
         <img src={{ asset('img/logo.png') }} alt="ads">
+    </div>
+
+    <div class="container px-10 py-5 mx-auto flex flex-wrap items-center justify-center">
         <p class="max-w-2xl mt-10 text-center dark:text-coolGray-400">KapeHingahan is a capstone project made by four
             students from University of Caloocan City. A coffee shop project that serves good quality concoctions of
             espresso-based drinks and mouth-watering food. It aims to provide excellent coffee and gracious service
@@ -14,12 +22,180 @@
     </div>
 
     <!-- Team -->
-    <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
-        <div class="text-center pb-12">
-            <h1 class="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-gray-900">
-                Check our awesome team members
-            </h1>
-        </div>
+    <div class="mb-16">
+        <dh-component>
+            <div class="container flex justify-center mx-auto pt-16">
+                <div>
+                    <h1 class="xl:text-4xl text-3xl text-center text-gray-800 font-extrabold pb-6 sm:w-full w-5/6 mx-auto">Check our awesome team members</span></h1>
+                </div>
+            </div>
+            <div class="w-full bg-gray-100 px-10 pt-10">
+                <div class="container mx-auto">
+                    <div role="list" aria-label="Behind the scenes People " class="lg:flex md:flex sm:flex items-center justify-center md:justify-around flex-wrap ">
+                        <div role="listitem" class="xl:w-1/4 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-2/5">
+                            <div class="rounded overflow-hidden shadow-md bg-white">
+                                <div class="absolute -mt-20 w-full flex justify-center">
+                                    <div class="h-32 w-32">
+                                        <img src="https://cdn.tuk.dev/assets/photo-1564061170517-d3907caa96ea.jfif" alt="Display Picture of Andres Berlin" role="img" class="rounded-full object-cover h-full w-full shadow-md" />
+                                    </div>
+                                </div>
+                                <div class="px-6 mt-16">
+                                    <h1 class="font-bold text-3xl text-center mb-1">Andres Berlin</h1>
+                                    <p class="text-gray-800 text-sm text-center">Chief Executive Officer</p>
+                                    <p class="text-center text-gray-600 text-base pt-3 font-normal">The CEO's role in raising a company's corporate IQ is to establish an atmosphere that promotes knowledge sharing and collaboration.</p>
+                                    <div class="w-full flex justify-center pt-5 pb-5">
+                                        <a href="javascript:void(0)" class="mx-5">
+                                            <div aria-label="Github" role="img">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#718096" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
+                                                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                                                </svg>
+                                            </div>
+                                        </a>
+                                        <a href="javascript:void(0)" class="mx-5">
+                                            <div aria-label="Twitter" role="img">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#718096" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter">
+                                                    <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+                                                </svg>
+                                            </div>
+                                        </a>
+                                        <a href="javascript:void(0)" class="mx-5">
+                                            <div aria-label="Instagram" role="img">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#718096" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram">
+                                                    <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+                                                    <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+                                                    <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+                                                </svg>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div role="listitem" class="xl:w-1/4 lg:mx-3 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-2/5">
+                            <div class="rounded overflow-hidden shadow-md bg-white">
+                                <div class="absolute -mt-20 w-full flex justify-center">
+                                    <div class="h-32 w-32">
+                                        <img src="https://cdn.tuk.dev/assets/photo-1530577197743-7adf14294584.jfif" alt="Display Picture of Silene Tokyo" role="img" class="rounded-full object-cover h-full w-full shadow-md" />
+                                    </div>
+                                </div>
+                                <div class="px-6 mt-16">
+                                    <h1 class="font-bold text-3xl text-center mb-1">Silene Tokyo</h1>
+                                    <p class="text-gray-800 text-sm text-center">Product Design Head</p>
+                                    <p class="text-center text-gray-600 text-base pt-3 font-normal">The emphasis on innovation and technology in our companies has resulted in a few of them establishing global benchmarks in product design and development.</p>
+                                    <div class="w-full flex justify-center pt-5 pb-5">
+                                        <a href="javascript:void(0)" class="mx-5">
+                                            <div aria-label="Github" role="img">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#718096" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
+                                                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                                                </svg>
+                                            </div>
+                                        </a>
+                                        <a href="javascript:void(0)" class="mx-5">
+                                            <div aria-label="Twitter" role="img">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#718096" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter">
+                                                    <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+                                                </svg>
+                                            </div>
+                                        </a>
+                                        <a href="javascript:void(0)" class="mx-5">
+                                            <div aria-label="Instagram" role="img">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#718096" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram">
+                                                    <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+                                                    <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+                                                    <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+                                                </svg>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div role="listitem" class="xl:w-1/4 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-2/5">
+                            <div class="rounded overflow-hidden shadow-md bg-white">
+                                <div class="absolute -mt-20 w-full flex justify-center">
+                                    <div class="h-32 w-32">
+                                        <img src="https://cdn.tuk.dev/assets/photo-1566753323558-f4e0952af115.jfif" alt="Display Picture of Johnson Stone" role="img" class="rounded-full object-cover h-full w-full shadow-md" />
+                                    </div>
+                                </div>
+                                <div class="px-6 mt-16">
+                                    <h1 class="font-bold text-3xl text-center mb-1">Johnson Stone</h1>
+                                    <p class="text-gray-800 text-sm text-center">Manager Development</p>
+                                    <p class="text-center text-gray-600 text-base pt-3 font-normal">Our services encompass the assessment and repair of property damage caused by water, fire, smoke, or mold. We can also be a part of the restoration.</p>
+                                    <div class="w-full flex justify-center pt-5 pb-5">
+                                        <a href="javascript:void(0)" class="mx-5">
+                                            <div aria-label="Github" role="img">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#718096" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
+                                                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                                                </svg>
+                                            </div>
+                                        </a>
+                                        <a href="javascript:void(0)" class="mx-5">
+                                            <div aria-label="Twitter" role="img">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#718096" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter">
+                                                    <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+                                                </svg>
+                                            </div>
+                                        </a>
+                                        <a href="javascript:void(0)" class="mx-5">
+                                            <div aria-label="Instagram" role="img">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#718096" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram">
+                                                    <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+                                                    <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+                                                    <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+                                                </svg>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div role="listitem" class="xl:w-1/4 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-2/5">
+                            <div class="rounded overflow-hidden shadow-md bg-white">
+                                <div class="absolute -mt-20 w-full flex justify-center">
+                                    <div class="h-32 w-32">
+                                        <img src="https://cdn.tuk.dev/assets/boy-smiling_23-2148155640.jpg" alt="Display Picture of Dean Jones" role="img" class="rounded-full object-cover h-full w-full shadow-md" />
+                                    </div>
+                                </div>
+                                <div class="px-6 mt-16">
+                                    <h1 class="font-bold text-3xl text-center mb-1">Dean Jones</h1>
+                                    <p class="text-gray-800 text-sm text-center">Principal Software Engineer</p>
+                                    <p class="text-center text-gray-600 text-base pt-3 font-normal">An avid open-source developer who loves to be creative and inventive. I have 20 years of experience in the field.</p>
+                                    <div class="w-full flex justify-center pt-5 pb-5">
+                                        <a href="javascript:void(0)" class="mx-5">
+                                            <div aria-label="Github" role="img">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#718096" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
+                                                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                                                </svg>
+                                            </div>
+                                        </a>
+                                        <a href="javascript:void(0)" class="mx-5">
+                                            <div aria-label="Twitter" role="img">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#718096" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter">
+                                                    <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+                                                </svg>
+                                            </div>
+                                        </a>
+                                        <a href="javascript:void(0)" class="mx-5">
+                                            <div aria-label="Instagram" role="img">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#718096" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram">
+                                                    <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+                                                    <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+                                                    <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+                                                </svg>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </dh-component>
+    </div>
+
+    {{-- <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6">
             <div class="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
                 <div class="mb-8">
@@ -63,6 +239,6 @@
             </div>
 
         </div>
-    </div>
+    </div> --}}
 
 </x-ecommerce-layout>
